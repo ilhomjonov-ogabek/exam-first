8a8edaf408fbfcce523989251a42be5341c81b6e
Ogabek <ilhomjonovo200@gmail.com>
2025-06-24 22:37:35 +0500

    third_task

--- main.cpp
+++ main.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+//int main() {
 
 
 /*n int a=true;
     cout << "Natija:" << (!a||a||!a) << endl; */
 
-    int a=false;
+ /*   int a=false;
     int b=true;
     int c=true;
     int d=false;
 
-    cout << " Natija:" << ((a && !b) || (c || d) && !(a || b)) << endl;
+    cout << " Natija:" << ((a && !b) || (c || d) && !(a || b)) << endl; */
 
 
 
-    return 0;
-}
+
+  //  return 0;
+//}
