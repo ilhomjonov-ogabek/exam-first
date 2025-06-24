fb339372d39618d229cf3439d4c86c35ca5f3971
Ogabek <ilhomjonovo200@gmail.com>
2025-06-24 22:33:39 +0500

    third_task

---
+++ third_task.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ int a,b;
+    cout << "Enter a number: "; cin >> a;
+    cout << "Enter another number: "; cin >> b;
+    if (a<0||b<0) {
+        cout << "true" << endl;
+    }else {
+        cout << "false" << endl;
+    }
+
+
+
+
+
+    return 0;
+}
